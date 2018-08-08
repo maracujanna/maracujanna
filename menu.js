743b8d381674adb3416f345f9a2e420b55d4d568
maracujanna <jannabrilyantova@gmail.com>
2018-08-07 22:21:02 -0300

    JQuery toggle add

---
+++ menu.js
@@ -0,0 +1,8 @@
+// console.log("hi");
+$(document).ready(function() {
+    $(".icon").on("click", function(){
+        $(".main-nav").toggleClass("open");
+        // $(".main-nav").animate({height: "200px"}, 300);
+
+    });
+});
