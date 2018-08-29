564810081e20e04e5e138304edf271ad9bf54497
Pedro <pedrodmbc@gmail.com>
2018-08-28 22:05:08 -0300

    nothing special

--- menu.js
+++ menu.js
@@ -1,12 +1,15 @@
 // console.log("hi");
 $(document).ready(function() {
-    $(".icon").on("click", function(){
-        $("body").toggleClass("open frezzed");
-        if ($('body').hasClass('open')) {
-            $(".main-nav").animate({height: $(window).height()}, 600);
-        } else {
-            $(".main-nav").animate({height: "0px"}, 600);
+    const   body = $('body'),
+            mainNav = $(".main-nav"),
+            iconMenuToggle = $(".icon");
 
+    iconMenuToggle.on("click", function(){
+        body.toggleClass("open frezzed");
+        if (body.hasClass('open')) {
+            mainNav.animate({height: $(window).height()}, 600);
+        } else {
+            mainNav.animate({height: "0px"}, 600);
         }
 
     });
