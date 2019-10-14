93f58c1a31deec2a72106dac26fce67b04511296
Pedro <pedrodmbc@gmail.com>
2019-10-14 09:21:06 -0300

    create new illustration page

---
+++ v2/projs/ilustracoes.php
@@ -0,0 +1,49 @@
+<!-- HEADER -->
+<?php 
+  $pageTitle = 'Ilustrações';
+	include_once("../includes/config.php");
+	include(ROOT_PATH . "includes/header.php");
+?>
+
+<!-- BEGIN -->
+<main class="main">
+	<div class="projeto">
+		<div class="about">      
+			<h4>Ilustrações</h4>
+		</div>  
+		<div class="proj-gallery-cont1">
+				<img src="<?php echo BASE_URL;?>images/illustration/Dancing_Girl.jpg" alt="Dancing Girl Illustration">
+		</div>
+		<div class="proj-gallery-cont1">
+				<img src="<?php echo BASE_URL;?>images/illustration/Bike-and-girl.jpg" alt="Illustration of a girl walking with a bike">
+		</div>
+		<div class="proj-gallery-cont1">
+				<img src="<?php echo BASE_URL;?>images/illustration/Carnival_Girls_Sun.jpg" alt="Carnival girl sun costume">
+		</div>
+		<div class="proj-gallery-cont1">
+				<img src="<?php echo BASE_URL;?>images/illustration/Carnival_Girls_Apple.jpg" alt="Carnival girl apple costume">
+		</div>
+		<div class="proj-gallery-cont1">
+				<img src="<?php echo BASE_URL;?>images/illustration/Parque-Lage.jpg" alt="Parque Lage">
+		</div>
+		<div class="proj-gallery-cont1">
+				<img src="<?php echo BASE_URL;?>images/illustration/Lisboa.jpg" alt="Lisboa Tram">
+		</div>
+		<div class="proj-gallery-cont1">
+				<img src="<?php echo BASE_URL;?>images/illustration/Carro.jpg" alt="Old mini">
+		</div>
+		<div class="proj-gallery-cont1">
+				<img src="<?php echo BASE_URL;?>images/illustration/1971.jpg" alt="Girl 1971 t-shirt">
+		</div>
+		<div class="proj-gallery-cont1">
+				<img src="<?php echo BASE_URL;?>images/illustration/Milia.jpg" alt="Milia">
+		</div>
+		<div class="proj-gallery-cont1">
+				<img src="<?php echo BASE_URL;?>images/illustration/boyandflours.jpg" alt="Boy red flours">
+		</div>
+	</div>    
+	
+</main>
+	
+<!-- FOOTER -->
+<?php include(ROOT_PATH ."includes/footer.php");?>
